d1af0b232384c3d7db947535966f1f70d20bcf46
SIU CHOI <77402407+siwoo281@users.noreply.github.com>
2025-11-02 20:27:58 +0900

    Fix: Update break-even point calculations with realistic cost structure
    - Fixed cost: 판매가의 4배(소형), 6배(중형), 8배(대형)
    - Variable cost: 판매가의 약 65% (항공+숙박+티켓+기타)
    - 손익분기점: 11-23명으로 현실적인 수준으로 조정
    - 마진율: 35-39%로 일관성 있게 개선

--- data/market.js
+++ data/market.js
@@ -40,132 +40,132 @@ export const marketAnalysis = {
     profitability: {
         tomatina: {
             sellingPrice: 2200000,
-            fixedCost: 500000,
-            variableCost: 1390000,
-            totalCost: 1890000,
-            margin: 0.141,
-            breakEven: 15,
+            fixedCost: 2200000 * 6, // 중형 축제: 13,200,000
+            variableCost: 1430000, // 항공35% + 숙박20% + 티켓8% + 기타25% = 88%의 65%
+            totalCost: (2200000 * 6) + 1430000,
+            margin: ((2200000 - 1430000) / 2200000),
+            breakEven: Math.ceil((2200000 * 6) / (2200000 - 1430000)),
             expectedCustomers: 120,
-            annualRevenue: 37200000
+            annualRevenue: 2200000 * 120
         },
         oktoberfest: {
             sellingPrice: 2800000,
-            fixedCost: 600000,
-            variableCost: 1720000,
-            totalCost: 2320000,
-            margin: 0.171,
-            breakEven: 12,
+            fixedCost: 2800000 * 8, // 대형 축제: 22,400,000
+            variableCost: 1820000, // 항공35% + 숙박25% + 티켓15% + 기타25% = 약 65%
+            totalCost: (2800000 * 8) + 1820000,
+            margin: ((2800000 - 1820000) / 2800000),
+            breakEven: Math.ceil((2800000 * 8) / (2800000 - 1820000)),
             expectedCustomers: 180,
-            annualRevenue: 86400000
+            annualRevenue: 2800000 * 180
         },
         carnival: {
             sellingPrice: 3500000,
-            fixedCost: 800000,
-            variableCost: 2420000,
-            totalCost: 3220000,
-            margin: 0.223,
-            breakEven: 10,
+            fixedCost: 3500000 * 8, // 대형 축제: 28,000,000
+            variableCost: 2275000, // 항공45% + 숙박20% + 기타25% = 약 65%
+            totalCost: (3500000 * 8) + 2275000,
+            margin: ((3500000 - 2275000) / 3500000),
+            breakEven: Math.ceil((3500000 * 8) / (3500000 - 2275000)),
             expectedCustomers: 105,
-            annualRevenue: 81900000
+            annualRevenue: 3500000 * 105
         },
-        // 추가 축제 수익성 지표 (보정치 기반)
+        // 추가 축제 수익성 지표 (현실적인 비용 구조 반영)
         'harbin-ice': {
             sellingPrice: 1800000,
-            fixedCost: 7700000,
+            fixedCost: 1800000 * 4, // 소형 축제: 판매가의 4배 (7,200,000)
             variableCost: 1100000,
-            totalCost: 8800000,
-            margin: (1800000 - 8800000) / 1800000, // 음수도 허용
-            breakEven: Math.ceil(7700000 / (1800000 - 1100000)),
+            totalCost: (1800000 * 4) + 1100000,
+            margin: ((1800000 - 1100000) / 1800000),
+            breakEven: Math.ceil((1800000 * 4) / (1800000 - 1100000)),
             expectedCustomers: 85,
             annualRevenue: 1800000 * 85
         },
         'cherry-blossom': {
             sellingPrice: 1200000,
-            fixedCost: 6000000,
-            variableCost: 800000,
-            totalCost: 6800000,
-            margin: (1200000 - 6800000) / 1200000,
-            breakEven: Math.ceil(6000000 / (1200000 - 800000)),
+            fixedCost: 1200000 * 4, // 소형 축제: 4,800,000
+            variableCost: 750000,
+            totalCost: (1200000 * 4) + 750000,
+            margin: ((1200000 - 750000) / 1200000),
+            breakEven: Math.ceil((1200000 * 4) / (1200000 - 750000)),
             expectedCustomers: 90,
             annualRevenue: 1200000 * 90
         },
         fringe: {
             sellingPrice: 2500000,
-            fixedCost: 9000000,
-            variableCost: 1600000,
-            totalCost: 10600000,
-            margin: (2500000 - 10600000) / 2500000,
-            breakEven: Math.ceil(9000000 / (2500000 - 1600000)),
+            fixedCost: 2500000 * 6, // 중형 축제: 15,000,000
+            variableCost: 1625000,
+            totalCost: (2500000 * 6) + 1625000,
+            margin: ((2500000 - 1625000) / 2500000),
+            breakEven: Math.ceil((2500000 * 6) / (2500000 - 1625000)),
             expectedCustomers: 130,
             annualRevenue: 2500000 * 130
         },
         sonkgran: {
             sellingPrice: 1300000,
-            fixedCost: 4500000,
-            variableCost: 850000,
-            totalCost: 5350000,
-            margin: (1300000 - 5350000) / 1300000,
-            breakEven: Math.ceil(4500000 / (1300000 - 850000)),
+            fixedCost: 1300000 * 4, // 소형 축제: 5,200,000
+            variableCost: 845000,
+            totalCost: (1300000 * 4) + 845000,
+            margin: ((1300000 - 845000) / 1300000),
+            breakEven: Math.ceil((1300000 * 4) / (1300000 - 845000)),
             expectedCustomers: 120,
             annualRevenue: 1300000 * 120
         },
         'oktober-alt': {
             sellingPrice: 2200000,
-            fixedCost: 8000000,
-            variableCost: 1400000,
-            totalCost: 9400000,
-            margin: (2200000 - 9400000) / 2200000,
-            breakEven: Math.ceil(8000000 / (2200000 - 1400000)),
+            fixedCost: 2200000 * 6, // 중형 축제: 13,200,000
+            variableCost: 1430000,
+            totalCost: (2200000 * 6) + 1430000,
+            margin: ((2200000 - 1430000) / 2200000),
+            breakEven: Math.ceil((2200000 * 6) / (2200000 - 1430000)),
             expectedCustomers: 90,
             annualRevenue: 2200000 * 90
         },
         'dia-de-muertos': {
             sellingPrice: 2400000,
-            fixedCost: 8100000,
-            variableCost: 1500000,
-            totalCost: 9600000,
-            margin: (2400000 - 9600000) / 2400000,
-            breakEven: Math.ceil(8100000 / (2400000 - 1500000)),
+            fixedCost: 2400000 * 6, // 중형 축제: 14,400,000
+            variableCost: 1560000,
+            totalCost: (2400000 * 6) + 1560000,
+            margin: ((2400000 - 1560000) / 2400000),
+            breakEven: Math.ceil((2400000 * 6) / (2400000 - 1560000)),
             expectedCustomers: 100,
             annualRevenue: 2400000 * 100
         },
         holi: {
             sellingPrice: 1600000,
-            fixedCost: 6000000,
-            variableCost: 1000000,
-            totalCost: 7000000,
-            margin: (1600000 - 7000000) / 1600000,
-            breakEven: Math.ceil(6000000 / (1600000 - 1000000)),
+            fixedCost: 1600000 * 4, // 소형 축제: 6,400,000
+            variableCost: 1040000,
+            totalCost: (1600000 * 4) + 1040000,
+            margin: ((1600000 - 1040000) / 1600000),
+            breakEven: Math.ceil((1600000 * 4) / (1600000 - 1040000)),
             expectedCustomers: 110,
             annualRevenue: 1600000 * 110
         },
         tomorrowland: {
             sellingPrice: 3800000,
-            fixedCost: 12000000,
-            variableCost: 2600000,
-            totalCost: 14600000,
-            margin: (3800000 - 14600000) / 3800000,
-            breakEven: Math.ceil(12000000 / (3800000 - 2600000)),
+            fixedCost: 3800000 * 8, // 대형 축제: 30,400,000
+            variableCost: 2470000,
+            totalCost: (3800000 * 8) + 2470000,
+            margin: ((3800000 - 2470000) / 3800000),
+            breakEven: Math.ceil((3800000 * 8) / (3800000 - 2470000)),
             expectedCustomers: 140,
             annualRevenue: 3800000 * 140
         },
         'up-alaaf': {
             sellingPrice: 2600000,
-            fixedCost: 9900000,
-            variableCost: 1700000,
-            totalCost: 11600000,
-            margin: (2600000 - 11600000) / 2600000,
-            breakEven: Math.ceil(9900000 / (2600000 - 1700000)),
+            fixedCost: 2600000 * 6, // 중형 축제: 15,600,000
+            variableCost: 1690000,
+            totalCost: (2600000 * 6) + 1690000,
+            margin: ((2600000 - 1690000) / 2600000),
+            breakEven: Math.ceil((2600000 * 6) / (2600000 - 1690000)),
             expectedCustomers: 95,
             annualRevenue: 2600000 * 95
         },
         'lantern-taiwan': {
             sellingPrice: 1100000,
-            fixedCost: 4000000,
-            variableCost: 700000,
-            totalCost: 4700000,
-            margin: (1100000 - 4700000) / 1100000,
-            breakEven: Math.ceil(4000000 / (1100000 - 700000)),
+            fixedCost: 1100000 * 4, // 소형 축제: 4,400,000
+            variableCost: 715000,
+            totalCost: (1100000 * 4) + 715000,
+            margin: ((1100000 - 715000) / 1100000),
+            breakEven: Math.ceil((1100000 * 4) / (1100000 - 715000)),
             expectedCustomers: 80,
             annualRevenue: 1100000 * 80
         }
